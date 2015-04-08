bb78d555d4880e787022e52cc62de42ba9d5df76
xhuberty <x.huberty@thecodingmachine.com>
2015-04-08 16:51:23 +0200

    Add coding to migrated from database.patcher 1.0 to 2.0

--- src/install.php
+++ src/install.php
@@ -18,9 +18,16 @@ InstallUtils::init(InstallUtils::$INIT_APP);
 $moufManager = MoufManager::getMoufManager();
 
 // Let's create the table.
-$dbConnection = $moufManager->getInstance('dbalConnection');
+$dbConnection = $moufManager->get('dbalConnection');
 /* @var $dbConnection ConnectionInterface */
 
+$existingPatches = $moufManager->findInstances("Mouf\\Database\\Patcher\\DatabasePatch");
+$dbConnectionDescriptor = $moufManager->getInstanceDescriptor('dbalConnection');
+foreach($existingPatches as $existingPatche){
+    $patchIntance = $moufManager->getInstanceDescriptor($existingPatche);
+    $patchIntance->getProperty('dbalConnection')->setValue($dbConnectionDescriptor);
+}
+$moufManager->rewriteMouf();
 $dbConnection->exec(file_get_contents(__DIR__.'/../database/create_patches_table.sql'));
 
 // Finally, let's continue the install
