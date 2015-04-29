1e5d8f72ad1e5302cb5caf9fb5f4f53584bfdf5e
Thomas Carli <t.carli@thecodingmachine.com>
2015-04-29 12:24:17 +0200

    Switch database structure from enum to varchar

--- database/create_patches_table.sql
+++ database/create_patches_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `patches` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `unique_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `status` enum('awaiting','applied','skipped','error') COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `exec_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `error_message` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`),
